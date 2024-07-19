599e50fe040954016ab6f622c12fe59ea993a438
Valet128 <98827302+Valet128@users.noreply.github.com>
2024-07-19 22:55:53 +0300

    Update cities_list.py

--- weather/main/static/main/files/cities_list.py
+++ weather/main/static/main/files/cities_list.py
@@ -1,8 +1,12 @@
+import json
 
-with open("main/static/main/files/cities.txt", 'r', encoding='utf-8') as file:
-    str1 = file.read()
-    cities_list = str1.split()
-
+with open("main/static/main/files/cities.json", encoding='utf-8') as file:
+    result = json.load(file)
+    
+cities_list = []
+for i in result['city']:
+    if i['name'] not in cities_list:
+        cities_list.append(i['name'])
 cities_dict = {
     'cities': cities_list
 }
